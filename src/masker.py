74a8a1c5ea791e3ac43f80aa979215c06998d74c
nikhilvutla <nikhilreddyvutla@gmail.com>
2023-07-11 14:48:15 +0530

    Fix masker

--- src/masker.py
+++ src/masker.py
@@ -1,8 +1,6 @@
 import hashlib
 
 def mask_pii(data):
-    # implement the masking logic
-    # Here we use SHA256 to mask data
     masked_data = data.copy()
     masked_data["device_id"] = hashlib.sha256(data["device_id"].encode()).hexdigest()
     masked_data["ip"] = hashlib.sha256(data["ip"].encode()).hexdigest()
